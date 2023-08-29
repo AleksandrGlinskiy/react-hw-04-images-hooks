f7ca02f6d0508165ebeee667255795374ad42649
AleksandrGlinskiy <tarieboe1993@gmail.com>
2023-08-29 10:59:58 +0300

    reffactoring component App with hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,45 @@
-import { Component } from 'react';
+import {useState } from 'react';
 import css from './App.module.css';
 import { ImageGallery } from './ImageGallery/ImageGallery';
 import { Searchbar } from './Searchbar/Searchbar';
 
-export class App extends Component {
-  state = {
-    searchText: '',
-    currentPage: 1
-    
+export function App () {
+  
+  const [searchText, setSearchText] = useState('');
+  const [currentPage, setCurrentPage] = useState('');
+  
+ 
+
+  const handleSearchbar = searchText => {
+    setSearchText({ searchText });
   };
 
-  handleSearchbar = searchText => {
-    this.setState({ searchText });
-  };
-
-  render() {
+  
     return (
       <div className={css.App}>
-        <Searchbar onSubmit={this.handleSearchbar} />
-        <ImageGallery searchText={this.state.searchText} currentPage={this.state.currentPage} />
+        <Searchbar onSubmit={handleSearchbar} />
+        <ImageGallery searchText={searchText} currentPage={currentPage} />
       </div>
     );
   }
-}
+
+// export class App extends Component {
+//   state = {
+//     searchText: '',
+//     currentPage: 1
+    
+//   };
+
+//   handleSearchbar = searchText => {
+//     this.setState({ searchText });
+//   };
+
+//   render() {
+//     return (
+//       <div className={css.App}>
+//         <Searchbar onSubmit={this.handleSearchbar} />
+//         <ImageGallery searchText={this.state.searchText} currentPage={this.state.currentPage} />
+//       </div>
+//     );
+//   }
+// }
